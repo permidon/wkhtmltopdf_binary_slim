dfc3ebaeb2ab8c52a041771c59fd89892f1102b0
Roman Perminov <permidon@me.com>
2026-07-08 15:38:16 -0400

    update testing steps

--- test/test_with_docker.rb
+++ test/test_with_docker.rb
@@ -1,15 +1,7 @@
 require 'minitest/autorun'
 
-def macos?
-  ENV['RUNNER_OS'] && ENV['RUNNER_OS'] == 'macOS'
-end
-
-def arm?
-  ENV['ARM']
-end
-
 class WithDockerTest < Minitest::Test
-  SETUP = (`docker compose build --no-cache` unless macos?)
+  SETUP = `docker compose build --no-cache`
 
   def test_debian_12
     test_on_x86_and_arm with: 'debian_12'
@@ -22,11 +14,11 @@ class WithDockerTest < Minitest::Test
   private
 
   def test_on_x86(with:)
-    test_on_docker(with: with) if !macos? && !arm?
+    test_on_docker(with: with)
   end
 
   def test_on_x86_and_arm(with:)
-    test_on_docker(with: with) unless macos?
+    test_on_docker(with: with)
   end
 
   def test_on_docker(with:)
