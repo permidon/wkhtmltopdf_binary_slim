bc2817498cdbad2349ceeebd7795fe06d56a79a1
Roman Perminov <permidon@me.com>
2026-07-08 15:17:04 -0400

    remove centos 8 support

--- test/test_with_docker.rb
+++ test/test_with_docker.rb
@@ -1,36 +1,24 @@
 require 'minitest/autorun'
 
-def macos?
-  ENV['RUNNER_OS'] && ENV['RUNNER_OS'] == 'macOS'
-end
-
-def arm?
-  ENV['ARM']
-end
-
 class WithDockerTest < Minitest::Test
   SETUP = (`docker compose build --no-cache` unless macos?)
-
-  def test_centos_8
-    test_on_x86 with: 'centos_8'
-  end
 
   def test_debian_12
     test_on_x86_and_arm with: 'debian_12'
   end
 
-  def test_amazonlinux_23
-    test_on_x86 with: 'amazonlinux_23'
+  def test_amzn_2023
+    test_on_x86 with: 'amzn_2023'
   end
 
   private
 
   def test_on_x86(with:)
-    test_on_docker(with: with) if !macos? && !arm?
+    test_on_docker(with: with)
   end
 
   def test_on_x86_and_arm(with:)
-    test_on_docker(with: with) unless macos?
+    test_on_docker(with: with)
   end
 
   def test_on_docker(with:)
