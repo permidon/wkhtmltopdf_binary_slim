255710d80a6cbc4a9a9968f674a68f1b4cb42974
Roman Perminov <permidon@me.com>
2026-08-06 10:11:53 -0400

    add AL2023 arm support

--- test/test_with_docker.rb
+++ test/test_with_docker.rb
@@ -1,29 +1,17 @@
 require 'minitest/autorun'
 
-def arm?
-  ENV['ARM']
-end
-
 class WithDockerTest < Minitest::Test
   SETUP = `docker compose build --no-cache`
 
   def test_debian_12
-    test_on_x86_and_arm with: 'debian_12'
+    test_on_docker with: 'debian_12'
   end
 
   def test_amzn_2023
-    test_on_x86 with: 'amzn_2023'
+    test_on_docker with: 'amzn_2023'
   end
 
   private
-
-  def test_on_x86(with:)
-    test_on_docker(with: with) unless arm?
-  end
-
-  def test_on_x86_and_arm(with:)
-    test_on_docker(with: with)
-  end
 
   def test_on_docker(with:)
     assert_match(/wkhtmltopdf 0\.12\.6(.1)? \(with patched qt\)/, `docker compose run --rm #{with}`.strip)
